e535680a0a7465402292abd3c0dc3ceb73dd9d0a
Rijal Asepnugroho <rijal.asep.nugroho@gmail.com>
2023-07-15 01:14:00 +0700

    [FEAT][#30] modul users

---
+++ app/Controllers/Users.php
@@ -0,0 +1,211 @@
+<?php
+
+namespace App\Controllers;
+use App\Models\UserModel;
+
+class Users extends BaseController
+{
+    public function index()
+    {
+        if (empty($this->session->get('user_id'))) return redirect("login");
+        return view('users/index');
+    }
+
+    public function create()
+    {
+        if (empty($this->session->get('user_id'))) return redirect("login");
+        
+        if ($this->request->is('post')) {
+            $validation =  \Config\Services::validation();
+            $validation->setRules(['name' => 'required']);
+            $validation->setRules(['email' => 'required|valid_email|is_unique[users.email]']);
+            $isDataValid = $validation->withRequest($this->request)->run();
+
+            if($isDataValid){
+                $password = $this->generatePassword(10);
+                $email = $this->request->getPost('email', FILTER_SANITIZE_EMAIL);
+                $userModel = new UserModel();
+                $userModel->insert([
+                    "name" => $this->request->getPost('name', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+                    "email" => $email,
+                    "password" => password_hash($password, PASSWORD_DEFAULT),
+                ]);
+                $user = $userModel->findByEmail($email);
+                $user['password'] = $password;
+                $this->sendEmail($user);
+            
+                return redirect('users');
+            }
+        }
+        return view('users/create');
+    }
+
+    public function update($id)
+    {
+        if (empty($this->session->get('user_id'))) return redirect("login");
+        
+        $userModel = new UserModel();
+        $data['user'] = $userModel->findById($id);
+        if ($this->request->is('post') || $this->request->is('put')) {
+            $validation =  \Config\Services::validation();
+            $validation->setRules(['name' => 'required']);
+            $validation->setRules(['is_actived' => 'required']);
+            $isDataValid = $validation->withRequest($this->request)->run();
+
+            if($isDataValid){
+                $userModel->updateById($id, [
+                    "name" => $this->request->getPost('name', FILTER_SANITIZE_FULL_SPECIAL_CHARS),
+                    "is_actived" => $this->request->getPost('is_actived') === 'on' ? true : false,
+                ]);
+                
+                return redirect('users');
+            }
+        }
+        return view('users/update', $data);
+    }
+
+    public function delete($id)
+    {
+        if (empty($this->session->get('user_id'))) return redirect("login");
+        
+        if ($this->request->is('post') || $this->request->is('delete')) {
+            $userModel = new UserModel();
+            $user = $userModel->findById($id);
+            if (isset($user['id']) && !empty($user['id'])) {
+                $userModel->deleteById($id);
+            }
+        }
+    }
+
+    public function verification($id)
+    {
+        $userModel = new UserModel();
+        $user = $userModel->findById($id);
+        if (isset($user['id']) && !empty($user['id']) && !$user['is_verified']) {
+            $userModel->verification($id);
+        }
+        
+        return redirect('login');
+    }
+
+    public function list()
+    {
+        if (empty($this->session->get('user_id'))) return redirect("login");
+        
+        $params = $this->request->getGet(null, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $draw = $params['draw'];
+        $offset = $params['start'];
+        $limit = $params['length']; // Rows display per page
+        $columnIndex = isset($params['order'])? $params['order'][0]['column']:null; // Column index
+        $order = ($columnIndex || $columnIndex === '0')?$params['columns'][$columnIndex]['data']:null; // Column name
+        $sort = $order?$params['order'][0]['dir']:null; // asc or desc
+        $search = $params['search']['value']; // Search value
+        
+        $userModel = new UserModel();
+        $count = $userModel->count($search);
+        $data = [
+            "draw" => intval($draw),
+            "iTotalRecords" => $count,
+            "iTotalDisplayRecords" => $count,
+            "data" => $userModel->list($search, $offset, $limit, $order, $sort)
+        ];
+        return json_encode($data);
+    }
+
+    public function logout()
+    {
+        if (empty($this->session->get('user_id'))) return redirect('login');
+        $this->session->destroy();
+        return redirect('login');
+    }
+
+    public function login()
+    {
+        if (!empty($this->session->get('user_id'))) return redirect("users");
+        
+        $data["error"] = "";
+        if ($this->request->is('post')) {
+            $validation =  \Config\Services::validation();
+            $validation->setRules(['email' => 'required|valid_email']);
+            $validation->setRules(['password' => 'required']);
+            $isDataValid = $validation->withRequest($this->request)->run();
+
+            if($isDataValid){
+                $email = $this->request->getPost('email', FILTER_SANITIZE_EMAIL);
+                $password = $this->request->getPost('password');
+                $password = $password ? $password : "";
+                $userModel = new UserModel();
+                $user = $userModel->findByEmail($email);
+                if (!isset($user['id'])) {
+                    $this->session->setFlashdata('error', 'invalid ID');
+                } else if (!$user['is_actived']){
+                    $this->session->setFlashdata('error', 'inactived user');
+                } else if (!$user['is_verified']){
+                    $this->session->setFlashdata('error', 'email is not verified');
+                } else if (!password_verify($password, $user['password'])){
+                    $this->session->setFlashdata('error', 'Invalid password or username');
+                } else {  
+                    $this->session->set('user_id', $user['id']);
+                    return redirect('users');
+                }
+                $data['error'] = $this->session->getFlashdata('error');
+            }
+        }
+        return view('users/login', $data);
+    }
+
+    private function sendEmail($user)
+    {
+        $url = base_url('users/'.$user['id'].'/verification');
+        $message = "
+            <p>Welcome to SIAKAD Mr. {$user['name']}.</p>
+            <p>Anda telah didaftarkan dalam sistem kami, silahkan masuk dengan menggunakan login:<br/>
+                Username : {$user['email']}<br/>
+                Password : {$user['password']}
+            </p>
+            <p>Untuk memverifikasi email Anda, klik link berikut: {$url}</p>
+        ";
+        
+        $this->email->initialize($this->emailConfig());
+        $this->email->setFrom(getenv('email_config_SMTPUser'), getenv('email_config_senderName'));
+        $this->email->setTo($user['email']);
+        $this->email->setSubject('Email Verification');
+        $this->email->setMessage($message);
+
+        return $this->email->send();
+    }
+
+    private function emailConfig()
+	{
+		$config['protocol'] = getenv('email_config_protocol');
+		$config['SMTPHost'] = getenv('email_config_SMTPHost');
+		$config['SMTPPort'] = getenv('email_config_SMTPPort');
+		$config['SMTPUser'] = getenv('email_config_SMTPUser');
+		$config['SMTPPass'] = getenv('email_config_SMTPPass');
+        $config['mailType'] = getenv('email_config_mailType');
+		
+		return $config;
+	}
+
+    private function generatePassword($length)
+    {
+        $specialChar = " !@#$%^&*\"'()+,-./:;<=>?[]\\_`{}|~";
+        $lowercase = "abcdefghijklmnopqrstuvwxyz";
+        $uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $number = "1234567890";
+
+        $randomString = '';
+        $randomString .= $specialChar[rand(0, strlen($specialChar)-1)];
+        $randomString .= $lowercase[rand(0, strlen($lowercase)-1)];
+        $randomString .= $uppercase[rand(0, strlen($uppercase)-1)];
+        $randomString .= $number[rand(0, strlen($number)-1)];
+        
+        $characters = $specialChar.$lowercase.$uppercase.$number;
+        $charactersLength = strlen($characters);
+        for ($i = 0; $i < ($length-4); $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+
+        return str_shuffle($randomString);
+    }
+}
---
+++ app/Views/users/update.php
@@ -0,0 +1,46 @@
+<?= $this->extend('layouts/page_layout') ?>
+<?= $this->section('content') ?>
+        <div class="right_col" role="main" style="min-height: 3790px;">
+          <div class="">
+            <div class="page-title">
+              <div class="title_left">
+                <h3>Users</h3>
+              </div>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_content">
+                  <?= validation_list_errors() ?>
+
+                  <?= form_open("users/{$user['id']}/update"); ?>
+                  <div class="item form-group">
+                      <label class="col-form-label col-md-3 col-sm-3 label-align" for="name">Name <span class="required">*</span></label>
+                      <div class="col-md-6 col-sm-6 ">
+                        <input type="text" id="name" name="name" required="required" class="form-control " value="<?= $user['name'] ?>" >
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                    <label class="col-form-label col-md-3 col-sm-3 label-align" for="is_actived">Is Actived <span class="required">*</span></label>
+                      <div class="col-md-9 col-sm-9 ">
+                        <div class="">
+                          <input name="is_actived" type="checkbox" class="js-switch" <?= $user['is_actived']?"checked":""; ?> data-switchery="true" style="display: none;">
+                        </div>
+                      </div>
+                    </div>
+                    <div class="item form-group">
+                      <div class="col-md-6 col-sm-6 offset-md-3">
+                        <button class="btn btn-primary" type="submit">Submit</button>
+                      </div>
+                    </div>
+                  <?= form_close() ?>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+<?= $this->endSection() ?>
