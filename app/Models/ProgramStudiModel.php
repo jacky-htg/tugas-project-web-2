191e9ca12020b8cef8a83b746d7c5e4b3c0938ea
Juldhais Hengkyawan <juldhais@gmail.com>
2023-07-15 07:04:09 +0700

    Revamp ProgramStudiModel

--- app/Models/ProgramStudiModel.php
+++ app/Models/ProgramStudiModel.php
@@ -1,33 +1,55 @@
 <?php
-// ProgramStudiModel.php
-
 namespace App\Models;
-
 use CodeIgniter\Model;
+use CodeIgniter\Database\Query;
 
-class ProgramStudiModel extends Model {
-    
-    protected $table = 'program_studi';
-    protected $primaryKey = 'ID';
-    protected $allowedFields = ['Nama', 'ProgramPendidikan', 'Akreditasi', 'SKAkreditasi'];
-    
-    public function getProgramStudi() {
-        return $this->findAll();
+class ProgramStudiModel extends Model
+{
+  protected $table = 'program studi';
+  protected $primaryId = 'id';
+  protected $allowedFields = ['nama', 'program_pendidikan', 'akreditasi', 'sk_akreditasi'];
+
+  public function list($search, $offset, $limit, $order, $sort)
+  {
+    $query = $this->select('id, nama, program_pendidikan, akreditasi, sk_akreditasi');
+    if ($search) {
+      $query = $query->like('nama', $search)->orLike('program_pendidikan', $search);
     }
-    
-    public function getProgramStudiById($id) {
-        return $this->find($id);
+    if (!empty($order) && !empty($sort)) {
+      $query = $query->orderBy($order, $sort);
     }
-    
-    public function insertProgramStudi($data) {
-        return $this->insert($data);
+    return $query->findAll($limit, $offset);
+  }
+
+  public function count($search)
+  {
+    if ($search) {
+      return $this->like('nama', $search)->orLike('program_pendidikan', $search)->countAllResults();
     }
-    
-    public function updateProgramStudi($id, $data) {
-        return $this->update($id, $data);
-    }
-    
-    public function deleteProgramStudi($id) {
-        return $this->delete($id);
-    }
-}
+    return $this->countAllResults();
+  }
+
+  public function findByNama($nama)
+  {
+    return $this->select('id, nama, program_pendidikan, akreditasi, sk_akreditasi')
+                ->where('nama', $nama)
+                ->first();
+  }
+
+  public function findById($id)
+  {
+    return $this->select('id, nama, program_pendidikan, akreditasi, sk_akreditasi')
+                ->where('id', $id)
+                ->first();
+  }
+
+  public function updateById($id, $data)
+  {
+    return $this->where('id', $id)->update(null, $data);
+  }
+
+  public function deleteById($id)
+  {
+    return $this->where('id', $id)->delete();
+  }
+} 
