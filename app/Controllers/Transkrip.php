46f7eb58cbbb123861fa38259fc7ce8dec4e6d58
Theodore Verrill Kendrick <erick6d30n@gmail.com>
2023-07-18 11:33:58 +0700

    [FEAT][21] Membuat API Transkrip Nilai

--- app/Controllers/Transkrip.php
+++ app/Controllers/Transkrip.php
@@ -5,6 +5,27 @@ use App\Models\TranskripModel;
 
 class Transkrip extends BaseController
 {
+    protected $transkripNilaiModel;
+
+    public function __construct()
+    {
+        $this->transkripNilaiModel = new TranskripModel();
+    }
+
+    public function index()
+    {
+        return view("transkrip/index");
+    }
+
+    public function delete($id)
+    {
+        // Menghapus data melalui API
+        $client = \Config\Services::curlrequest();
+        $response = $client->delete('http://localhost:8080/api/transkripnilai/' . $id);
+
+        return redirect()->to('transkrip');
+    }
+
     public function create()
     {
         // if (empty($this->session->get('user_id'))) return redirect("login");
@@ -56,4 +77,39 @@ class Transkrip extends BaseController
         return view('transkrip/update', $data);
     }
 
+    public function list()
+    {
+        // if (empty($this->session->get('user_id'))) return redirect("login");
+        
+        $params = $this->request->getGet(null, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $draw = isset($params['draw'])?$params['draw']:1;
+        $offset = isset($params['start'])?$params['start']:0;
+        $limit = isset($params['length'])?$params['length']:10; // Rows display per page
+        $columnIndex = isset($params['order'])? $params['order'][0]['column']:null; // Column index
+        $order = ($columnIndex || $columnIndex === '0')?$params['columns'][$columnIndex]['data']:null; // Column name
+        $sort = $order?$params['order'][0]['dir']:null; // asc or desc
+        $search = isset($params['search'])?$params['search']['value']:''; // Search value
+        
+        $transkripModel = new TranskripModel();
+        $count = $transkripModel->count($search);
+        $data = [
+            "draw" => intval($draw),
+            "iTotalRecords" => $count,
+            "iTotalDisplayRecords" => $count,
+            "data" => $transkripModel->list($search, $offset, $limit, $order, $sort)
+        ];
+        return json_encode($data);
+    }
+
+    public function lookup()
+    {
+        // if (empty($this->session->get('user_id'))) return redirect("login");
+        
+        $params = $this->request->getGet(null, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $search = isset($params['search'])?$params['search']:'';
+        
+        $transkripModel = new TranskripModel();
+        $data = $transkripModel->lookup($search);
+        return json_encode($data);
+    }
 }
