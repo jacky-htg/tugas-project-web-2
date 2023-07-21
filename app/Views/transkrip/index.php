cdf606cd50aa4a36f89773df88802f282e2b17c1
Rijal Asepnugroho <rijal.asep.nugroho@gmail.com>
2023-07-21 17:23:45 +0700

    change bg to green

--- app/Views/transkrip/index.php
+++ app/Views/transkrip/index.php
@@ -164,7 +164,7 @@
                   render: function(data, type, row) {
                     return '<div class="btn-group" role="group">' +
                       '<button data-id="' + row.DT_RowId + '" class="btn btn-success btn-sm updateBtn"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>' +
-                      '<button data-id="' + row.DT_RowId + '" class="btn btn-info btn-sm viewBtn"><i class="fa fa-file" aria-hidden="true"></i></button>' +
+                      '<button data-id="' + row.DT_RowId + '" class="btn btn-success btn-sm viewBtn"><i class="fa fa-file" aria-hidden="true"></i></button>' +
                       '<button data-id="' + row.DT_RowId + '" class="btn btn-danger btn-sm deleteBtn"><i class="fa fa-trash-o" aria-hidden="true"></i></button>' +
                       '</div>';
                   }
