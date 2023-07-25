9a9dd50b0780e1ddf5f59401f8df0f3903fe59c1
Jejen Suwaji <suwaji.jejen@gmail.com>
2023-07-25 15:59:59 +0700

    #61 issued

--- app/Models/TranskripModel.php
+++ app/Models/TranskripModel.php
@@ -46,10 +46,11 @@ class TranskripModel extends Model
 
   public function findById($id)
   {
-    return $this->select('transkrip_nilai.id, transkrip_nilai.taruna, transkrip_nilai.ijazah as ijazah_id, ijazah.nomer_ijazah as ijazah, transkrip_nilai.program_studi as program_studi_id, program_studi.nama as program_studi')
+    return $this->select('transkrip_nilai.id, transkrip_nilai.taruna as taruna_id, taruna.nama as taruna, transkrip_nilai.ijazah as ijazah_id, ijazah.nomer_ijazah as ijazah, transkrip_nilai.program_studi as program_studi_id, program_studi.nama as program_studi')
                 ->where('transkrip_nilai.id', $id)
                 ->join('program_studi', 'program_studi.id = transkrip_nilai.program_studi')
                 ->join('ijazah', 'ijazah.id = transkrip_nilai.ijazah')
+                ->join('taruna', 'taruna.id = transkrip_nilai.taruna')
                 ->first();
   }
 
